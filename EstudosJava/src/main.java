bd61997f920ca14aadcc05793303d1bdc5dd530c
hayghlander <hayghlanderuitec@gmail.com>
2026-07-30 23:42:43 -0300

    do-while

--- EstudosJava/src/main.java
+++ EstudosJava/src/main.java
@@ -3,9 +3,11 @@ public class main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+        int k = 0;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k < 5);
 
-        }
     }
 }
