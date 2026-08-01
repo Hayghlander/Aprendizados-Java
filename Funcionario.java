25e165f5b3c9ff505b0f15199a60dc35beb00575
hayghlander <hayghlanderuitec@gmail.com>
2026-07-31 22:05:55 -0300

    atualizando

--- Funcionario.java
+++ Funcionario.java
@@ -20,7 +20,7 @@ public class Funcionario {
         System.out.println("Nome: " + funcionario1.nome);
         System.out.println("Cargo: " + funcionario1.cargo);
         System.out.println("Idade: " + funcionario1.idade);
-
+        System.out.println("------------------");
         System.out.println("Nome: " + funcionario2.nome);
         System.out.println("Cargo: " + funcionario2.cargo);
         System.out.println("Idade: " + funcionario2.idade);
