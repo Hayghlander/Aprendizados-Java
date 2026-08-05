91e8a3b5cd3dd904b11b7b4a6e7cc2796136cb19
hayghlander <hayghlanderuitec@gmail.com>
2026-08-05 08:18:57 -0300

    Sobrecarga de construtores

--- Funcionario.java
+++ Funcionario.java
@@ -5,24 +5,25 @@ public class Funcionario {
     String cargo;
     int idade;
 
+    Funcionario(String nomeInit, String cargoInit, int idadeInit) {
+        nome = nomeInit;
+        cargo = cargoInit;
+        idade = idadeInit;
+    }
+
+    Funcionario(String nomeInit) {
+        nome = nomeInit;
+    }
+
+    Funcionario() {
+
+    }
+
     public static void main(String[] args) {
 
-        Funcionario funcionario1 = new Funcionario();
-        funcionario1.nome = "João";
-        funcionario1.cargo = "Desenvolvedor";
-        funcionario1.idade = 30;
-
-        Funcionario funcionario2 = new Funcionario();
-        funcionario2.nome = "Maria";
-        funcionario2.cargo = "Gerente";
-        funcionario2.idade = 45;
-
-        System.out.println("Nome: " + funcionario1.nome);
-        System.out.println("Cargo: " + funcionario1.cargo);
-        System.out.println("Idade: " + funcionario1.idade);
-        System.out.println("------------------");
-        System.out.println("Nome: " + funcionario2.nome);
-        System.out.println("Cargo: " + funcionario2.cargo);
-        System.out.println("Idade: " + funcionario2.idade);
+        Funcionario funcionario = new Funcionario("Maria", "Gerente", 50);
+        System.out.println("Nome: " + funcionario.nome);
+        System.out.println("Cargo: " + funcionario.cargo);
+        System.out.println("Idade: " + funcionario.idade);
     }
 }
