7a6d6adfa8ed8cd10e2cc1b9a30193b5fd6194a2
hayghlander <hayghlanderuitec@gmail.com>
2026-08-07 08:49:36 -0300

    Modificador PRIVATE

--- src/empresa/dados/Funcionario.java
+++ src/empresa/dados/Funcionario.java
@@ -2,13 +2,17 @@ package empresa.dados;
 
 public class Funcionario {
 
-    String nome;
+    private String nome;
     double salario;
 
-    public Funcionario(String nomeInit, double salarioInit) {
+    private Funcionario(String nomeInit, double salarioInit) {
         nome = nomeInit;
         salario = salarioInit;
         System.out.println("Nome: " + nome);
         System.out.println("Salario: " + salario);
+    }
+
+    private Funcionario() {
+        Funcionario func = new Funcionario("lucas", 2000);
     }
 }
